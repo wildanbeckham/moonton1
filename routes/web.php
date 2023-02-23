8ea69446bca7bffa6ec4a6b5954db1436386ed6c
wildanbeckham <wildanbeckham5@gmail.com>
2023-02-23 08:05:50 +0700

    feat: Integrasi Detail Movie

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\User\DashboardController;
+use App\Http\Controllers\User\MovieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +23,8 @@ Route::redirect('/', '/login');
 
 Route::middleware(['auth', 'role:user'])->prefix('dashboard')->name('user.dashboard.')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('index');
+
+    Route::get('movie/{movie:slug}', [MovieController::class, 'show'])->name('movie.show');
 });
 
 // Route::middleware('auth')->group(function () {
